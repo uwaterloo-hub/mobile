64b38d4aa15fd159c59fa5880d765834dd2cc918
Steven <wusteven815@gmail.com>
2023-03-03 17:44:15 -0500

    Fix nav animation

--- src/navigators/HomeNavigator.js
+++ src/navigators/HomeNavigator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Pressable } from "react-native";
 import { BottomNavigation } from "react-native-paper";
 
@@ -7,15 +7,19 @@ import MoreScreen from "../screens/MoreScreen";
 import RoomsScreen from "../screens/RoomsScreen";
 import ScheduleScreen from "../screens/ScheduleScreen";
 import WeatherScreen from "../screens/WeatherScreen";
+import { Settings } from "../settings";
 
 
 export default function HomeNavigator({ navigation }) {
+
+    const settings = useContext(Settings);
+    useEffect(() => settings.setNavigator(navigation), []);
 
     const [index, setIndex] = React.useState(2);
     const [routes] = React.useState([
         { key: "weather", title: "Weather", focusedIcon: "cloud", unfocusedIcon: "cloud-outline"},
         { key: "maps", title: "Maps", focusedIcon: "compass", unfocusedIcon: "compass-outline" },
-        { key: "schedule", title: "Schedule", focusedIcon: "calendar" },
+        { key: "schedule", title: "Schedule", focusedIcon: "calendar", unfocusedIcon: "calendar-outline" },
         { key: "rooms", title: "Rooms", focusedIcon: "map-marker", unfocusedIcon: "map-marker-outline" },
         { key: "more", title: "More", focusedIcon: "dots-horizontal", unfocusedIcon: "dots-horizontal" }
     ]);
@@ -25,7 +29,7 @@ export default function HomeNavigator({ navigation }) {
         maps: MapsScreen,
         schedule: ScheduleScreen,
         rooms: RoomsScreen,
-        more: () => <MoreScreen navigation={navigation}/>
+        more: MoreScreen
     });
 
     return (
