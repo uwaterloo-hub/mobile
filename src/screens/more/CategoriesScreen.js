cd609eb88bbfded8a81c263ce6e8299e7895fded
Steven <wusteven815@gmail.com>
2023-03-05 00:20:01 -0500

    Draft category look

--- src/screens/more/CategoriesScreen.js
+++ src/screens/more/CategoriesScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Avatar, Surface, Text } from "react-native-paper";
 
 import Screen from "../../components/Screen";
 import { titleMap } from "../details/detailMaps";
@@ -9,11 +9,34 @@ import { titleMap } from "../details/detailMaps";
 export default function CategoriesScreen({ navigation }) {
 
     return (
-        <Screen navigation={navigation} title={titleMap.Categories}>
-            <View style={{ alignItems: "center", flex: 1, justifyContent: "center" }}>
-                <Text>Categories screen</Text>
-            </View>
+        <Screen navigation={navigation} title={titleMap.Categories} style={{ alignItems: "center" }}>
+            <Surface elevation={2} style={styles.container}>
+                <Avatar.Icon icon="cloud-outline" size={40} style={styles.icon} />
+                <Text style={styles.label}>Weather</Text>
+                <View style={{ flex: 1 }} />
+                <Avatar.Icon icon="menu-up" size={40} style={styles.icon} />
+                <Avatar.Icon icon="menu-down" size={40} style={styles.icon} />
+            </Surface>
         </Screen>
     );
 
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        borderRadius: 12,
+        flexDirection: "row",
+        margin: 5,
+        padding: 10,
+        width: "90%"
+    },
+    icon: {
+        backgroundColor: "transparent"
+    },
+    label: {
+        fontSize: 16,
+        marginLeft: 10
+    }
+})
