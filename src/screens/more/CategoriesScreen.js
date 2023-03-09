869b9ac79fa188715d867f69e164059e05c4bb95
Steven <wusteven815@gmail.com>
2023-03-08 19:03:20 -0500

    Template icon buttons

--- src/screens/more/CategoriesScreen.js
+++ src/screens/more/CategoriesScreen.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Avatar, Surface, Text } from "react-native-paper";
+import { Avatar, Surface, Text, TouchableRipple } from "react-native-paper";
 
 import Screen from "../../components/Screen";
 import { titleMap } from "../details/detailMaps";
 
+
+function IconButton({ icon, onPress }) {
+
+    return (
+        <View style={styles.buttonParent}>
+            <TouchableRipple onPress={onPress} rippleColor="#ffffff40">
+                <Avatar.Icon icon={icon} size={40} style={styles.icon} />
+            </TouchableRipple>
+        </View>
+    );
+
+}
 
 export default function CategoriesScreen({ navigation }) {
 
@@ -14,8 +26,8 @@ export default function CategoriesScreen({ navigation }) {
                 <Avatar.Icon icon="cloud-outline" size={40} style={styles.icon} />
                 <Text style={styles.label}>Weather</Text>
                 <View style={{ flex: 1 }} />
-                <Avatar.Icon icon="menu-up" size={40} style={styles.icon} />
-                <Avatar.Icon icon="menu-down" size={40} style={styles.icon} />
+                <IconButton icon="menu-up" onPress={() => console.log("up")} />
+                <IconButton icon="menu-down" onPress={() => console.log("down")} />
             </Surface>
         </Screen>
     );
@@ -24,6 +36,15 @@ export default function CategoriesScreen({ navigation }) {
 
 
 const styles = StyleSheet.create({
+
+    buttonParent: {
+        alignItems: "center",
+        backgroundColor: "transparent",
+        borderRadius: 20,
+        justifyContent: "center",
+        marginRight: 8,
+        overflow: "hidden"
+    },
     container: {
         alignItems: "center",
         borderRadius: 12,
@@ -37,4 +58,5 @@ const styles = StyleSheet.create({
         fontSize: 16,
         marginLeft: 10
     }
+
 })
